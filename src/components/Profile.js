14375f7d27bc839686e2793755ff6684d018efac
priyabanait <priyabanait91@gmail.com>
2023-08-22 20:41:15 +0530

    Edit User Details

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,76 @@
-import React from 'react'
+import React,{useContext, useRef,useEffect} from 'react'
 import './Profile.css';
 import { FaGithub } from "react-icons/fa6";
 import {FiGlobe} from "react-icons/fi";
+import AuthContext from './store/AuthContext';
+
 export default function Profile() {
+const context=useContext(AuthContext);
+    const nameRef=useRef();
+     const imageRef=useRef();
+    async function submitHandler(event){
+        event.preventDefault();
+        
+     const res= await fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAQYXLrWSQR8lxbt1sc-ye5bGOTDsYKzQM',{
+        method:'POST',
+      
+        body:JSON.stringify({
+          displayName:nameRef.current.value,
+          photoUrl:imageRef.current.value,
+            idToken:context.token,
+            returnSecureToken:true}),
+            
+            headers:{
+                'Content-Type':'application/json'
+              }
+      })
+        if(res.ok){
+    alert('profile updated sucessfully')
+    console.log(res.json());
+    nameRef.current.value = "";
+    imageRef.current.value = "";
+        }else{
+          
+      alert('failed')
+            
+        }  
+    }
+    useEffect(() => {
+      const updateAccount = async () => {
+        try {
+          const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyAQYXLrWSQR8lxbt1sc-ye5bGOTDsYKzQM', {
+            method: 'POST',
+            body: JSON.stringify({
+              idToken: context.token,
+            }),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          });
+  
+          if (response.ok) {
+            const responseData = await response.json();
+            const users = responseData.users;
+        console.log(users);
+       
+            if (users && users.length > 0) {
+              const [{ displayName, photoUrl }] = users;
+              nameRef.current.value = displayName;
+              imageRef.current.value = photoUrl;
+            }
+          } else {
+            alert('Response not okay');
+          }
+        } catch (error) {
+          console.error('Error updating account:', error);
+        }
+      }
+  
+      if (context.token) {
+        updateAccount();
+      }
+    }, [context.token]);
+    
   return (
     <div>
       <div className='profile'>
@@ -12,18 +80,18 @@ export default function Profile() {
         </div>
       </div>
       <hr></hr>
-    <div className='details'>
+    <form className='details' onSubmit={submitHandler}>
         <div className='contact'>
         <h1>Contact Details</h1>
         <button className='cancel'>Cancel</button></div>
         <div className='form'>
         <label htmlFor='name'><span><FaGithub/></span>Name:</label>
-        <input type='text' name='name' className='input'></input>
+        <input type='text' name='name' className='input' ref={nameRef}></input>
         <label htmlFor='photo'><span><FiGlobe/></span>Profile Photo URL:</label>
-        <input type='text' name='name' className='input'></input>
+        <input type='text' name='img' className='input' ref={imageRef}></input>
         </div>
         <button className='btn'>Update</button>
-    </div>
+    </form>
     </div>
   )
 }
--- src/components/store/AuthContext.js
+++ src/components/store/AuthContext.js
@@ -1,34 +1,39 @@
-import React,{useState} from 'react'
+import React, { useState, useEffect } from 'react';
 
-const AuthContext=React.createContext({
-    token:"",
-    isLogin:false,
-    login:(token)=>{},
-    logout:()=>{}
-})
-export const AuthContextProvider=(props)=>{
-    const initialToken=localStorage.getItem('token');
-    const[token,setToken]=useState(initialToken);
-    const userIsLoggedIn=!!token;
+const AuthContext = React.createContext({
+  token: "",
+  isLogin: false,
+  login: (token) => {},
+  logout: () => {}
+});
 
-    const loginHandler=(token)=>{
-        setToken(token);
-        localStorage.setItem('token',token)
-    }
-    
-    const logoutHandler=()=>{
-        setToken(null);
-        localStorage.removeItem('token')
-    }
-    
-    const contextValue={
-        token:token,
-        isLogin:userIsLoggedIn,
-        login:loginHandler,
-        logout:logoutHandler
-    }
-return <AuthContext.Provider value={contextValue}>
-{props.children}
-</AuthContext.Provider>
+export const AuthContextProvider = (props) => {
+  const initialToken = localStorage.getItem('token');
+  const [token, setToken] = useState(initialToken);
+  const userIsLoggedIn = !!token;
+
+  const loginHandler = (token) => {
+    setToken(token);
+    localStorage.setItem('token', token);
+  }
+
+  const logoutHandler = () => {
+    setToken(null);
+    localStorage.removeItem('token');
+  }
+
+  const contextValue = {
+    token: token,
+    isLogin: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler
+  }
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 }
+
 export default AuthContext;
